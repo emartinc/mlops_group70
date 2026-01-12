862af8db0fa305b3fb153d2ef25efce5bfa74923
Estela Martín Cebrián <estelamartincebrian@gmail.com>
2026-01-12 14:28:02 +0100

    init cookiecutter project

---
+++ src/exercises_estela/train.py
@@ -0,0 +1,10 @@
+from exercises_estela.model import Model
+from exercises_estela.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
