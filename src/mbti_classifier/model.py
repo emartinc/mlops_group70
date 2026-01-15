2f4e141cf1b6a8725550cb710ae257b4d69c460a
Estela Martín Cebrián <estelamartincebrian@gmail.com>
2026-01-15 19:34:34 +0100

    Fix training crash, clean up src folder, and add data1 script

--- src/mbti_classifier/model.py
+++ src/mbti_classifier/model.py
@@ -1,16 +1,36 @@
-from torch import nn
+import logging
 import torch
+from transformers import DistilBertForSequenceClassification
 
-class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(1, 1)
+# configuring the loggs
+logger = logging.getLogger(__name__)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+### dummy model - testing 
+def build_model(num_labels: int = 2):
+    model_name = "distilbert-base-uncased"
+    logger.info(f"Loading architecture: {model_name}")
+
+    # Usamos la clase de Hugging Face que ya tiene la "cabeza" de clasificación
+    model = DistilBertForSequenceClassification.from_pretrained(
+        model_name, 
+        num_labels=num_labels
+    )
+    
+    return model
 
 if __name__ == "__main__":
-    model = Model()
-    x = torch.rand(1)
-    print(f"Output shape of model: {model(x).shape}")
+    logging.basicConfig(level=logging.INFO) ### prueba 
+    
+    # 1. model
+    model = build_model()
+    
+    # 2. Cfalse dta to test the model - sanity checkkk 
+    # we simualate a sentence of 10 words 
+    dummy_input = torch.randint(0, 1000, (1, 10)) 
+    
+    # 3. a forward pass 
+    output = model(dummy_input)
+    
+    print(f"correctly loaded model: {type(model).__name__}")
+    print(f"Output shape (Logits): {output.logits.shape}") 
+    # Debería salir [1, 2] -> 1 frase, 2 probabilidades (Clase 0 vs Clase 1)
